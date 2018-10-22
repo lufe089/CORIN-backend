40e6ee9ce7a254b64777518c976e3b9162d1318a
lufe0 <lufe089@gmail.com>
2018-10-21 20:06:37 -0500

    FIXED: se modifica la vista clientViewSet para quitar el order by created_at para corregir error en el consumo del servicio

--- Apps/encuestador/views.py
+++ Apps/encuestador/views.py
@@ -70,7 +70,7 @@ class CompanyViewSet (viewsets.ModelViewSet):
 
 class ClientViewSet (viewsets.ModelViewSet):
     serializer_class = ClientSerializer
-    queryset = Client.objects.all().order_by('-created_at')
+    queryset = Client.objects.all()
     # queryset = Client.objects.all().order_by('-created_at')
 
 class ConfigSurveysByClientsViewSet (viewsets.ModelViewSet):
